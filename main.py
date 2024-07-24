cd76d2cbe26c983f46ff9a80d075f3e2b2f2e692
minhosong88 <hominsong@naver.com>
2024-07-24 09:54:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from wanted_job_search import WantedJobSearch
 
 if __name__ == "__main__":
     search = WantedJobSearch()
-    search.add_keyword("flutter")
-    search.add_keyword("nextjs")
-    search.add_keyword("kotlin")
+
+    # Add your keywords here
+    # examples of keywords: flutter, nextjs, kotlin, python, etc.
+    keywords = input("Enter keywords separated by commas: ").split(',')
+    for keyword in keywords:
+        search.add_keyword(keyword.strip())
 
     search.save_to_excel()
