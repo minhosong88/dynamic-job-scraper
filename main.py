4634b127cb957c2ca3219a6c9cda59d7c8fb422a
minhosong88 <hominsong@naver.com>
2024-08-02 20:53:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -78,15 +78,3 @@ def save():
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-# if __name__ == "__main__":
-#     search = WantedJobSearch()
-
-#     # Add your keywords here
-#     # examples of keywords: flutter, nextjs, kotlin, python, etc.
-#     keywords = input("Enter keywords separated by commas: ").split(',')
-#     for keyword in keywords:
-#         search.add_keyword(keyword.strip())
-
-#     search.save_to_excel()
